f9493f435b1046879c940d14be48c78ad030a985
LxpHomePc <lin_bo_lin@126.com>
2019-05-27 20:59:57 +0800

    20190527

--- workspace/src/github.com/dudulangjiao/SpeechApplicationGolang/hello/hello.go
+++ workspace/src/github.com/dudulangjiao/SpeechApplicationGolang/hello/hello.go
@@ -3,275 +3,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"github.com/dudulangjiao/SpeechApplicationGolang/stringutil"
 )
 
-// NOT逻辑门
-func Not(input byte) byte {
-	switch input {
-	case '1':
-		return '0'
-	case '0':
-		return '1'
-	default:
-		return 'e'
-	}
-}
-
-// AND逻辑门
-func And(inputFirst, inputSecond byte) byte {
-	z := string(inputFirst) + string(inputSecond)
-	switch z {
-	case "11":
-		return '1'
-	case "01", "10", "00":
-		return '0'
-	default:
-		return 'e'
-	}
-}
-
-// OR逻辑门
-func Or(inputFirst, inputSecond byte) byte {
-	z := string(inputFirst) + string(inputSecond)
-	switch z {
-	case "11", "01", "10":
-		return '1'
-	case "00":
-		return '0'
-	default:
-		return 'e'
-	}
-}
-
-// XOR逻辑门 或 一位加法器
-func XOr(inputFirst, inputSecond byte) byte {
-	and := And(inputFirst, inputSecond)
-	notAnd := Not(and)
-	or := Or(inputFirst, inputSecond)
-	output := And(notAnd, or)
-	return output
-}
-
-// 半加器
-func HalfAdder(inputFirst, inputSecond byte) (byte, byte) {
-	sum := XOr(inputFirst, inputSecond)
-	carry := And(inputFirst, inputSecond)
-	return carry, sum
-}
-
-// 全加器
-func FullAdder(inputFirst, inputSecond,
-	inputThird byte) (byte, byte) {
-	c1, s := HalfAdder(inputFirst, inputSecond)
-	c2, sum := HalfAdder(s, inputThird)
-	carry := Or(c1, c2)
-	return carry, sum
-}
-
-// 8位加法器
-func EightBitAdder(inputFirst, inputSecond string) (byte, string) {
-	output := "00000000"
-	outputByte := []byte(output)
-	var carry byte
-	inputFirstByte := []byte(inputFirst)
-	inputSecondByte := []byte(inputSecond)
-	carry, outputByte[7] = HalfAdder(inputFirstByte[7],
-		inputSecondByte[7])
-	for i := 6; i >= 0; i-- {
-		carry, outputByte[i] = FullAdder(carry, inputFirst[i],
-			inputSecond[i])
-	}
-	output = string(outputByte)
-	return carry, output
-}
-
-// 门锁 Gated Latch
-func GatedLatch(DataInput, WriteEnable, returnCircuit byte) byte {
-	a := Or(And(DataInput, WriteEnable), returnCircuit)
-	b := Not(And(Not(DataInput), WriteEnable))
-	c := And(a, b)
-	return c
-}
-
-// 8位寄存器 8-BIT Register
-type EightBitRegister struct {
-	WriteEnable   byte
-	DataInput     string
-	returnCircuit [8]byte
-}
-
-func NewEightBitRegister() *EightBitRegister {
-	var result = new(EightBitRegister)
-	for i := 0; i < 8; i++ {
-		result.returnCircuit[i] = '0'
-	}
-	return result
-}
-
-func (e *EightBitRegister) ReadWrite() string {
-	DataInputByte := []byte(e.DataInput)
-	result := "00000000"
-	resultByte := []byte(result)
-	for i := 0; i < 8; i++ {
-		resultByte[i] = GatedLatch(DataInputByte[i], e.WriteEnable,
-			e.returnCircuit[i])
-		e.returnCircuit[i] = resultByte[i]
-	}
-	result = string(resultByte)
-	return result
-}
-
-// 矩阵型门锁
-func MatrixGatedLatch(rowWire, columnWire, dataInOut, writeEnable,
-	readEnable, returnCircuit byte) (byte, byte) {
-	a := And(rowWire, columnWire)
-	storedValue := GatedLatch(dataInOut, And(a, writeEnable),
-		returnCircuit)
-	cInput := And(a, readEnable)
-	output := And(storedValue, cInput)
-	return storedValue, output
-}
-
-type MatrixGatedLatchObject struct {
-	returnCircuit byte
-}
-
-func NewMatrixGatedLatch() *MatrixGatedLatchObject {
-	var result = new(MatrixGatedLatchObject)
-	result.returnCircuit = '0'
-	return result
-}
-
-func (m *MatrixGatedLatchObject) ReadWrite(RowWire, ColumnWire,
-	DataInOut, WriteEnable, ReadEnable byte) (byte, byte) {
-	storedValue, output := MatrixGatedLatch(RowWire, ColumnWire,
-		DataInOut, WriteEnable, ReadEnable, m.returnCircuit)
-	m.returnCircuit = storedValue
-	/*
-		fmt.Println("RowWire:", string(RowWire),
-			"ColumnWire:", string(ColumnWire),
-			"WriteEnable:", string(WriteEnable),
-			"ReadEnable:", string(ReadEnable),
-			"DataInOut:", string(DataInOut),
-			"returnCircuit:", string(storedValue))
-		fmt.Println("DataInOut:", string(output))
-	*/
-	return storedValue, output
-}
-
-// 256个门锁集合
-type TwoHundredFiftySixBitMemory struct {
-	// 256个门锁集合
-	MatrixGatedLatchSet [16][16]MatrixGatedLatchObject
-}
-
-func New256BitMemory() *TwoHundredFiftySixBitMemory {
-	var result = new(TwoHundredFiftySixBitMemory)
-	for i := 0; i < 16; i++ {
-		for j := 0; j < 16; j++ {
-			result.MatrixGatedLatchSet[i][j] = *NewMatrixGatedLatch()
-		}
-	}
-	return result
-}
-
-func (t *TwoHundredFiftySixBitMemory) ReadWrite(Data, WriteEnable,
-	ReadEnable byte, EightBitAddress string) (byte, byte) {
-	rowAddress, _ := strconv.ParseInt(EightBitAddress[0:4],
-		2, 10)
-
-	columnAddress, _ := strconv.ParseInt(EightBitAddress[4:8],
-		2, 10)
-
-	storedValue, output := t.
-		MatrixGatedLatchSet[rowAddress][columnAddress].
-		ReadWrite('1', '1', Data, WriteEnable,
-			ReadEnable)
-	/*
-		fmt.Println("EightBitAddress:", EightBitAddress,
-			"WriteEnable:", string(WriteEnable),
-			"ReadEnable:", string(ReadEnable),
-			"Data:", string(Data), "storedValue:", string(storedValue))
-	*/
-	return storedValue, output
-}
-
-// 8位可寻址内存 EightBitAddressableMemory
-type EightBitAddressableMemory struct {
-	// 8个256位内存
-	TwoHundredFiftySixBitMemorySet [8]TwoHundredFiftySixBitMemory
-}
-
-func NewEightBitAddressableMemory() *EightBitAddressableMemory {
-	var result = new(EightBitAddressableMemory)
-	for i := 0; i < 8; i++ {
-		result.TwoHundredFiftySixBitMemorySet[i] = *New256BitMemory()
-	}
-	return result
-}
-
-func (a *EightBitAddressableMemory) ReadWrite(WriteEnable,
-	ReadEnable byte, EightBitData,
-	EightBitAddress string) (string, string) {
-	m := []byte(EightBitData)
-	result := "00000000"
-	storeValue := "00000000"
-	resultByte := []byte(result)
-	storeValueByte := []byte(storeValue)
-	for i := 0; i < 8; i++ {
-		storeValueByte[i], resultByte[i] = a.
-			TwoHundredFiftySixBitMemorySet[i].
-			ReadWrite(m[i], WriteEnable, ReadEnable, EightBitAddress)
-	}
-	result = string(resultByte)
-	storeValue = string(storeValueByte)
-	fmt.Println("\nWriteEnable:", string(WriteEnable),
-		"ReadEnable:", string(ReadEnable),
-		"EightBitData:", EightBitData,
-		"EightBitAddress:", EightBitAddress)
-	fmt.Println("storeValue:", storeValue, "DataInOut:", result)
-	return storeValue, result
-}
-
 func main() {
-	/*
-		Address := "00011110"
-		rowAddress, _ := strconv.ParseInt(Address[0:4],  2,
-			10)
-		columnAddress, _ := strconv.ParseInt(Address[4:8], 2,
-			10)
-		fmt.Println(rowAddress)
-		fmt.Println(columnAddress)
-
-		dd1 := "01011110"
-		dd2 := "11110001"
-		fmt.Println("8位加法器")
-		fmt.Println(EightBitAdder(dd1, dd2))
-
-		fmt.Println("\n8位寄存器")
-		ww := NewEightBitRegister()
-
-		// 1 Input:"00011001" WriteEnable:'0' Result:"00000000"
-		ww.DataInput = "00011001"
-		ww.WriteEnable = '0'
-		fmt.Println("Input:", ww.DataInput, "Write:",
-			string(ww.WriteEnable), "---Result:", ww.ReadWrite())
-
-		fmt.Println("\n矩阵型门锁")
-		juzhen := NewMatrixGatedLatch()
-
-		// 1 RC:11 WriteRead:00 DAtaInOut:0
-		juzhen.ReadWrite('1', '1',
-			'0', '0', '0')
-
-		hhhh := New256BitMemory()
-		_, hul := hhhh.ReadWrite('1', '1', '0',
-			"00111101")
-		fmt.Println("\n256位内存")
-		fmt.Println(hul)
-	*/
-	eightBitAddressableMem := NewEightBitAddressableMemory()
+	eightBitAddressableMem := stringutil.NewEightBitAddressableMemory()
 
 	fmt.Println("\n8位可寻址内存")
 	eightBitAddressableMem.ReadWrite('1', '0',
@@ -287,4 +23,55 @@ func main() {
 
 	eightBitAddressableMem.ReadWrite('0', '0',
 		"00000111", "11000110")
+
+	fourBitAddressableMem := stringutil.NewFourBitAddressableMemory()
+
+	fmt.Println("\n4位可寻址内存")
+	fourBitAddressableMem.ReadWrite('1', '0',
+		"11110001", "1100")
+
+	fourBitAddressableMem.ReadWrite('0', '1',
+		"00000001", "1100")
+	fourBitAddressableMem.ReadWrite('1', '1',
+		"00000001", "1100")
+
+	fourBitAddressableMem.ReadWrite('1', '0',
+		"00000011", "1100")
+
+	fourBitAddressableMem.ReadWrite('0', '0',
+		"00000111", "1100")
+
+	fmt.Println("\nCPU")
+	ram := stringutil.NewFourBitAddressableMemory()
+	ram.ReadWrite('1', '0',
+		"00101110", "0000") // ADDRESS:0
+	ram.ReadWrite('1', '0',
+		"00011111", "0001") // ADDRESS:1
+	ram.ReadWrite('1', '0',
+		"10000100", "0010") // ADDRESS:2
+	ram.ReadWrite('1', '0',
+		"01001101", "0011") // ADDRESS:3
+	ram.ReadWrite('1', '0',
+		"00000011", "1110") // ADDRESS:14
+	ram.ReadWrite('1', '0',
+		"00001110", "1111") // ADDRESS:15
+
+	aRegister := stringutil.NewEightBitRegister()
+	bRegister := stringutil.NewEightBitRegister()
+	cRegister := stringutil.NewEightBitRegister()
+	dRegister := stringutil.NewEightBitRegister()
+
+	instructionAddressRegister := stringutil.NewEightBitRegister()
+	instructionRegister := stringutil.NewEightBitRegister()
+
+	_, ramOutput0 := ram.ReadWrite('0', '1',
+		"00000000",
+		instructionAddressRegister.ReadWrite('0',
+			"00000000"))
+	instructionRegister.ReadWrite('1', ramOutput0)
+
+	fmt.Println(ramOutput0)
+	fmt.Println("\n", ram, aRegister, bRegister, cRegister, dRegister,
+		instructionAddressRegister, instructionRegister)
+
 }
